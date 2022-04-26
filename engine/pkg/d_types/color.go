b4964f09f0665a3b11c69c8d547864b58f18d955
droidkfx <kam157190@gmail.com>
2022-04-26 12:45:42 -0400

    Correct color

--- engine/pkg/d_types/color.go
+++ engine/pkg/d_types/color.go
@@ -3,8 +3,8 @@ package d_types
 //goland:noinspection GoSnakeCaseUsage,GoUnusedGlobalVariable
 var (
 	Color_RED   = ColorRGB{R: 1.0}
-	Color_BLUE  = ColorRGB{G: 1.0}
-	Color_GREEN = ColorRGB{B: 1.0}
+	Color_BLUE  = ColorRGB{B: 1.0}
+	Color_GREEN = ColorRGB{G: 1.0}
 )
 
 type ColorRGB struct {
